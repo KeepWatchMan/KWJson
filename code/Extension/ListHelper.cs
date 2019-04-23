d25229230f63ae2fe78fbaaad16ba4012b8dc212
lifeycn <crebox.zhang@gmail.com>
2019-04-23 14:26:52 +0800

    update

---
+++ code/Extension/ListHelper.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace Extension
+{
+    public static class ListHelper{
+        public static List<T> InsertionSort(this List<T> _list){
+            return _list;
+        }
+    }
+}
